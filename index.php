53d1ef9d1f77c4d88d2dac9c09463679ade95b1d
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-05-18 15:59:28 -0400

    Building Systems

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="Styles/main-style.css">
+<link rel="stylesheet" type="text/css" href="Styles/menu-style.css">
 <title>
 Dwarves Civ
 </title>
@@ -10,9 +11,52 @@ Dwarves Civ
 
 <body>
 
+<ul id="navbar">
+	<li><a href="#">Dwarves</a>
+	<ul>
+		<li><a href="#" id="NDwarf">New Dwarf</a></li>
+		<li><a href="#" id="NMiner">New Miner</a></li>
+		<li><a href="#" id="NFarmer">New Farmer</a></li>
+		<li><a href="#" id="NLogger">New Logger</a></li>
+		<li><a href="#" id="NBlacksmith">New Blacksmith</a></li>
+		<li><a href="#" id="NHunter">New Hunter</a></li>
+		<li><a href="#" id="NGuard">New Guard</a></li>
+		<li><a href="#" id="NShepherd">New Shepherd</a></li>
+		<li><a href="#" id="NBuilder">New Builder</a></li>
+		<li><a href="#" id="NBrickmaker">New Brickmaker</a></li>
+	</ul>
+	</li>
+	
+	<li><a href="#">Buildings</a>
+	<ul>
+		<li><a href="#" id="NHouse">New House</a></li>
+		<li><a href="#" id="NMine">New Mine</a></li>
+		<li><a href="#" id="NFarm">New Farm</a></li>
+		<li><a href="#" id="NMill">New Mill</a></li>
+		<li><a href="#" id="NSmithery">New Smithery</a></li>
+		<li><a href="#" id="NBarracks">New Barracks</a></li>
+		<li><a href="#" id="NField">New Field</a></li>
+		<li><a href="#" id="NBrickField">New Brick Field</a></li>
+	</ul>
+	</li>
+	
+	<li><a href="#">Tools</a>
+	<ul>
+		<li><a href="#" id="NPick">New Pick</a></li>
+		<li><a href="#" id="NHoe">New Hoe</a></li>
+		<li><a href="#" id="NAxe">New Axe</a></li>
+		<li><a href="#" id="NAnvil">New Anvil</a></li>
+		<li><a href="#" id="NHammer">New Hammer</a></li>
+		<li><a href="#" id="NSword">New Sword</a></li>
+		<li><a href="#" id="NBow">New Bow</a></li>
+		<li><a href="#" id="NShear">New Shear</a></li>
+		<li><a href="#" id="NBrickMold">New Brick Mold</a></li>
+	</ul>
+	</li>
+	
+</ul>
 
 <!-- Load Scipts -->
-
 <script src="Scripts/jquery.min.js" /></script>
 <script src="Scripts/buttons.js" /></script>
 <script src="Variables/variables.js" /></script>
